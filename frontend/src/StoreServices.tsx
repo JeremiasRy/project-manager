7c0e90f60b987c816d34dd661448c4597a34befc
ELNAUL99 <58483543+ELNAUL99@users.noreply.github.com>
2023-01-30 23:36:06 +0200

    update-endpoint

--- frontend/src/StoreServices.tsx
+++ frontend/src/StoreServices.tsx
@@ -8,11 +8,11 @@ class StoreServices {
     }
 
     async login (data:LoginDataType) {
-        return await axios.post('https://api.escuelajs.co/api/v1/auth/login', {'email': data.email, 'password': data.password});
+        return await axios.post('/login', {'email': data.email, 'password': data.password});
     }
 
     async createNewUser (data:RegisterDataType) {
-        return await axios.post('https://api.escuelajs.co/api/v1/users/', {'email': data.email, 'password': data.password})
+        return await axios.post('/register', {'email': data.email, 'password': data.password})
     }
 
 }
